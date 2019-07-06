39fb8586f9380dc6ee8a3e15da1cb7fddc9168f5
Drovoseek <drovosek2703@yandex.ru>
2019-07-06 19:27:36 +0500

    добавил страницы перевозчика, поставщика и заказчика

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
 
+  </div>
+</div>
+
 
 </body>
 </html>
+<script>
+    $(document).ready(function(){
+        var url = window.location.href;
+        url = (url.substr(url.indexOf('project/') - 1));
+        $('.nav-link[href="'+url+'"]').addClass('active');
+    });
+</script>
